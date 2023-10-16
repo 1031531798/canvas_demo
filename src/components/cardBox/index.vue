76b68ac31e1ad753d70d769bd096e2291e72b249
zijianlin <1031531798@qq.com>
2023-10-16 17:56:02 +0800

    feat(CardBox): 设置毛玻璃效果

--- src/components/cardBox/index.vue
+++ src/components/cardBox/index.vue
@@ -12,7 +12,7 @@
 import {defineProps} from 'vue'
 import {systemConfig} from '@/config/system'
 import Icon from '../iconSvg/icon.vue';
-const prefixCls = systemConfig.publicPrefixCls + '-card-box'
+const prefixCls = systemConfig.publicPrefixCls + '-CardBox'
 defineProps({
   title: String,
   icon: String
@@ -20,16 +20,21 @@ defineProps({
 </script>
 
 <style lang="scss" scoped>
-  .shete-card-box {
+  .shete-CardBox {
     border-radius: 20px;
-    border: 1px solid #888;
-    background-color: $--color-bg;
+    position: relative;
+    background-color: transparent;
+    backdrop-filter: blur(15px);
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3); /* 添加阴影效果 */
     &-title {
       display: flex;
       flex-direction: row;
       justify-content: flex-start;
       align-items: center;
+      font-size: 20px;
+      font-weight: bold;
       padding: 10px 20px;
+      color: $--text-color;
       span {
         margin-left: 10px;
       }
